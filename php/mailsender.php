998fbc2f5f4d3d9e95306f27880935fabd67c605
mwilchek <mwilchek@gmu.edu>
2017-05-17 22:42:07 -0400

    gh

--- php/mailsender.php
+++ php/mailsender.php
@@ -1,6 +1,6 @@
 <?php
 
-	$emailTo = "mwilchek@gmu.edu"; // Enter your email for feedbacks here 
+	$emailTo = 'mwilchek@gmu.edu'; // Enter your email for feedbacks here 
 	
 	$headers = "MIME-Version: 1.0\r\n";
 	$headers .= "Content-type: text/html; charset=utf-8\r\n";
